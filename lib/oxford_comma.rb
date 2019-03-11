5174dae56d405cfc03c35ee16b4496efdd2dc688
Pete A <peter.ayresmusic@gmail.com>
2019-03-11 02:03:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,10 +6,8 @@ def oxford_comma(array)
       array.join(" and ")
   elsif array.size == 3
           array[0]<<", "<<array[1]<<", "<<"and "<<array[2]
-        else
-           if array.size > 3
-            array[-2] << " and"
+        else array.size > 3
+          array[-2] << " and"
             array.join(",")
-               end
-            end
         end
+    end
