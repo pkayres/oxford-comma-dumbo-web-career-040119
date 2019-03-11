63b47c56ca617591e33b97e978189e60e58a687e
Pete A <peter.ayresmusic@gmail.com>
2019-03-11 02:33:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
 
-end
+def oxford_comma(array)
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+      array.join(" and ")
+  elsif array.size == 3
+          array[0]<<", "<<array[1]<<", "<<"and "<<array[2]
+    else array.size > 3
+         array[-1].prepend("and ")
+            array.join(", ")
+        end
+    end
