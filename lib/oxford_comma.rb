24a444f22dfac042f4b06950bb3cb45096a3847e
Pete A <peter.ayresmusic@gmail.com>
2019-03-11 01:39:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
 
-end
+def oxford_comma(array)
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+      array.join(" and ")
+  elsif array.size == 3
+          array[0]<<", "<<array[1]<<", "<<"and "<<array[2]
+  else
+         array.size > 3
+       array[0]<<", "<<array[1]<<", "<<array[2]<<", "<<array[3]<<", "<<"and "<<array[4]
+      end
+    end
+  
